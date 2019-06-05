8283c569751cc0da716266900c4e7aa3d7b747f2
Georges Labreche <georges.labreche@gmail.com>
2019-06-05 10:55:29 +0200

    More diurnal plots.

--- diurnal_irradiance_variations.R
+++ diurnal_irradiance_variations.R
@@ -23,6 +23,9 @@ G_eqs = c(Gh_eq, Gbh_eq, Gdh_eq)
 G_eqs_labels = c("global irradiance", "beam irradiance", "diffuse irradiance")
 G_eqs_cols = c("red", "blue", "green")
 
+# Tau list options
+taus_clear_day = c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0)
+taus_selected = c(0.1, 0.5, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, 6.0)
 
 # Areocentric Longitude values (deg).
 Ls_VE = 0       # Vernal Equinox - Dust Storm Season ends.
@@ -44,22 +47,28 @@ omega_u = 17 # [h]
 # Sequence of omega values to calculate irradiance.
 omega_seq = seq(omega_l, omega_u, 1)
 
-# Calculate irradiance.
+# Calculate irradiances.
+
+##############################################################################################################################
+# Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different areocentric longitudes. #
+##############################################################################################################################
 dev.new()
 par(mfrow=c(3,2))
 Ls_index = 1
 for(Ls in Ls_seq){
-  
+
   G_index = 1
   for(G_eq in G_eqs){
     G_seq = c()
-    
+
+    # Calculate the irradiance for each given parameters.
+    # Store the results in a sequence that will be used in the plot() function.
     for(omega in omega_seq){
       Z = Z_eq(Ls, omega, phi)
       G = G_eq(Ls, Z, tau)
       G_seq = c(G_seq, G)
     }
-    
+
     # Plot
     if(G_index == 1){
       plot(omega_seq, G_seq,
@@ -67,22 +76,176 @@ for(Ls in Ls_seq){
            ylim=c(0,550),
            pch=3,
            col=G_eqs_cols[G_index],
-           sub=paste(Ls_lbl_seq [Ls_index], " (Ls=", Ls, "°)", sep=""))
-      
+           sub=paste(Ls_lbl_seq [Ls_index], " (Ls = ", Ls, "°)", sep=""),
+           font.sub=2,
+           cex.sub=1.2)
+
       smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
       lines(smooth_line, col=G_eqs_cols[G_index])
     }else{
       smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
       lines(smooth_line, col=G_eqs_cols[G_index])
-      
+
       points(omega_seq, G_seq,
              pch=3,
              col=G_eqs_cols[G_index])
     }
-    
+
     G_index = G_index + 1
   }
-  
+
   Ls_index = Ls_index + 1
 }
-mtext(paste("Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface\n", paste("(τ=", tau, ", ϕ=", phi, "°N)")), side = 3, line = -3, outer = TRUE)
+mtext(paste("Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different areocentric longitudes\n", paste("(τ=", tau, ", ϕ=", phi, "°)", sep="")), side = 3, line = -3, outer = TRUE)
+
+######################################################################################################################
+# Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different optical depths. #
+######################################################################################################################
+
+# Select an areocentric longitude.
+Ls = Ls_A
+
+# Select optical depth tau factors.
+taus = taus_selected
+
+dev.new()
+par(mfrow=c(3,3))
+for(tau in taus){
+
+  G_index = 1
+  for(G_eq in G_eqs){
+    G_seq = c()
+
+    # Calculate the irradiance for each given parameters.
+    # Store the results in a sequence that will be used in the plot() function.
+    for(omega in omega_seq){
+      Z = Z_eq(Ls, omega, phi)
+      G = G_eq(Ls, Z, tau)
+      G_seq = c(G_seq, G)
+    }
+
+    # Plot
+    if(G_index == 1){
+      plot(omega_seq, G_seq,
+           xlab="Solar Time [h]", ylab="Gh [W/m2]",
+           ylim=c(0,550),
+           pch=3,
+           col=G_eqs_cols[G_index],
+           sub=paste("τ = ", tau, sep=""),
+           font.sub=2,
+           cex.sub=1.2)
+
+      smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
+      lines(smooth_line, col=G_eqs_cols[G_index])
+    }else{
+      smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
+      lines(smooth_line, col=G_eqs_cols[G_index])
+
+      points(omega_seq, G_seq,
+             pch=3,
+             col=G_eqs_cols[G_index])
+    }
+
+    G_index = G_index + 1
+  }
+}
+mtext(paste("Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different optical depths\n", paste("(Ls=", Ls, "°, ϕ=", phi, "°)", sep="")), side = 3, line = -3, outer = TRUE)
+
+
+######################################################################################################################
+# Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different optical depths. #
+######################################################################################################################
+
+# Select an areocentric longitude.
+Ls = Ls_A
+
+# Select an optical depth tau factor.
+tau = 0.5
+
+# Select latitudes
+phis = seq(-40, 40, 10)
+
+dev.new()
+par(mfrow=c(3,3))
+for(phi in phis){
+  new_plot_initialized = FALSE
+  
+  G_index = 1
+  for(G_eq in G_eqs){
+    G_seq = c()
+    
+    # Calculate the irradiance for each given parameters.
+    # Store the results in a sequence that will be used in the plot() function.
+    for(omega in omega_seq){
+      Z = Z_eq(Ls, omega, phi)
+      G = G_eq(Ls, Z, tau)
+      G_seq = c(G_seq, G)
+    }
+    
+    # Plot
+    if(length(omega_seq) == length(G_seq)){
+      if(G_index == 1){
+        plot(omega_seq, G_seq,
+             xlab="Solar Time [h]", ylab="Gh [W/m2]",
+             ylim=c(0, 600),
+             pch=3,
+             col=G_eqs_cols[G_index],
+             sub=paste("ϕ = ", phi, "°", sep=""),
+             font.sub=2,
+             cex.sub=1.2)
+        
+        smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
+        lines(smooth_line, col=G_eqs_cols[G_index])
+        
+        new_plot_initialized = TRUE
+      }else{
+          
+          tryCatch({
+            if(new_plot_initialized == TRUE){
+              smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
+              lines(smooth_line, col=G_eqs_cols[G_index])
+            
+              points(omega_seq, G_seq,
+                   pch=3,
+                   col=G_eqs_cols[G_index])
+            }else{
+               stop("New plot has not been initialized.")
+             }
+          },
+          warning = function(w) {
+            # Do nothing
+          },
+          error = function(e) {
+            # Enter here when following error occurs: plot.new has not been called yet
+            plot(omega_seq, G_seq,
+                 xlab="Solar Time [h]", ylab="Gh [W/m2]",
+                 ylim=c(0,550),
+                 pch=3,
+                 col=G_eqs_cols[G_index],
+                 sub=paste("ϕ = ", phi, sep=""),
+                 font.sub=2,
+                 cex.sub=1.2)
+            
+            smooth_line = smooth.spline(omega_seq, G_seq, spar=0.35)
+            lines(smooth_line, col=G_eqs_cols[G_index])
+            
+            new_plot_initialized = TRUE
+          },
+          finally = {
+            # Do nothing
+          })
+        
+      }
+    }else{
+      message(paste("Could not calculate ", G_eqs_labels[G_index] , " for latitude ϕ=", phi, "°", sep=""))
+    }
+    
+    if(G_index == 3){
+      G_index = 1
+    }else{
+      G_index = G_index + 1
+    }
+    
+  }
+}
+mtext(paste("Diurnal variation of global, beam, and diffuse irradiance on Mars horizontal surface for different latitudes\n", paste("(Ls=", Ls, "°, τ=", tau, "°)", sep="")), side = 3, line = -3, outer = TRUE)
