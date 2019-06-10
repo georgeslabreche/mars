86873a03533b76bf5656952a87b8597b1a1d43e0
Georges Labrèche <georges@opendatakosovo.org>
2019-06-10 15:10:01 +0200

    Added data point regarding Opportunity's last contact.

--- plots/optical_depth_variations.R
+++ plots/optical_depth_variations.R
@@ -8,10 +8,13 @@ tau_eq = dget(here("functions", "tau.R"))
 # tau expresson models.
 models = c(1, 2)
 
+Ls_opportunity_last_contact = 190
+phi_opportunity_last_contact = -2.28
+
 #################################################################################
 # Variation of optical depth with areocentric longitude at different latitudes. #
 #################################################################################
-phi_seq = seq(-30, 30, 10) # [deg]
+phi_seq = c(seq(-30, 30, 10), phi_opportunity_last_contact) # [deg]
 Ls_seq = seq(0, 360, 1) # [deg]
 
 dev.new()
@@ -20,6 +23,10 @@ for(model in models){
   
   phi_index = 1
   for(phi in phi_seq){
+    
+    # Emphasize opportunity last contact case.
+    lty = if(phi==phi_opportunity_last_contact) 2 else 1
+    
     tau_seq = c()
   
     # For some reason not working when doing tau_eq(phi, Ls_seq, 1).
@@ -37,16 +44,27 @@ for(model in models){
            sub=paste("Model ", model),
            font.sub=2,
            cex.sub=1.2,
-           type='l',
+           type="l",
+           lty=lty,
            col=wes_palette("FantasticFox1", length(phi_seq), type="continuous")[phi_index])
     }else{
-      lines(Ls_seq, tau_seq, type='l',
+      lines(Ls_seq, tau_seq, type='l', lty=lty,
             col=wes_palette("FantasticFox1", length(phi_seq), type="continuous")[phi_index])
     }
   
     phi_index = phi_index + 1
   
   }
+  
+  tau = tau_eq(phi_opportunity_last_contact, Ls_opportunity_last_contact, model)
+  
+  points(Ls_opportunity_last_contact, tau,
+         col=wes_palette("FantasticFox1", length(phi_seq), type="continuous")[phi_index-1])
+  
+  text(Ls_opportunity_last_contact, tau,
+       labels=paste("Opportunity Last Contact\nLs=", Ls_opportunity_last_contact, "°, ϕ= ", phi_opportunity_last_contact, "°\nPredicted τ=", round(tau, 2), ", Actual τ=", 10.8, sep=""),
+       cex=0.5,
+       pos=2)
   
   legend("topleft",
          paste("ϕ = ", phi_seq, "°", sep=""),
@@ -60,9 +78,10 @@ mtext("Variation of optical depth with areocentric longitude at different latitu
 ##################################################################################
 # Variation of optical depth with latitudes at different areocentric longitudes. #
 ##################################################################################
-phi_seq = seq(-90, 90, 1) # [deg]
+phi_seq = seq(-90, 90, 10)  # [deg]
 
 Ls_list = list(
+  "Ls = 190° (Opportunity Last Contact)" = Ls_opportunity_last_contact,
   #"Ls = 0°   (Vernal Equinox) " = 0,
   "Ls = 71°   (Aphelion)" = 71,
   #"Ls = 90°  (Summer Solstice)" = 90,
@@ -82,6 +101,10 @@ for(model in models){
   for(Ls in rev(Ls_seq)){
     tau_seq = c()
     
+    # Emphasize opportunity last contact case.
+    lty = if(Ls==Ls_opportunity_last_contact) 2 else 1
+    
+    
     # For some reason not working when doing tau_eq(phi_seq, Ls, 1).
     # So get taus iteratively.
     for(phi in phi_seq){
@@ -100,6 +123,7 @@ for(model in models){
            font.sub=2,
            cex.sub=1.2,
            type='l',
+           lty=lty,
            col=wes_palette("FantasticFox1", length(Ls_seq), type="continuous")[Ls_index])
       
       axis(1, seq(-90, 90, 10))
@@ -108,12 +132,23 @@ for(model in models){
       lines(phi_seq, tau_seq,
             yaxt='n',
             type='l',
+            lty=lty,
             col=wes_palette("FantasticFox1", length(Ls_seq), type="continuous")[Ls_index])
     }
     
     Ls_index = Ls_index + 1
     
   }
+  
+  tau = tau_eq(phi_opportunity_last_contact, Ls_opportunity_last_contact, model)
+  
+  points(phi_opportunity_last_contact, tau,
+         col=wes_palette("FantasticFox1", length(Ls_seq), type="continuous")[Ls_index-1])
+  
+  text(phi_opportunity_last_contact, tau,
+       labels=paste("Opportunity Last Contact\nLs=", Ls_opportunity_last_contact, "°, ϕ= ", phi_opportunity_last_contact, "°\nPredicted τ=", round(tau, 2), ", Actual τ=", 10.8, sep=""),
+       cex=0.5,
+       pos=4)
   
   legend("topright",
          paste(rev(names(Ls_list)), sep=""),
