0809aebb04a6f9c4e378b9dcb2275b57f5ce1c36
Georges Labrèche <georges@opendatakosovo.org>
2019-06-12 22:46:03 +0200

    Processed Viking data and initial refactoring into functions.

--- mer_tau.R
+++ mer_tau.R
@@ -1,9 +1,3 @@
-# Phoenix
-#   Dataset:      PHOENIX Atmospheric Opacity data.
-#   Desctiption:  Derived data from the SSI instrument measuring the atmospheric opacity for sols 1-151.
-#   Source:       PDS - Planetary Atmospheres Node
-#   URL:          https://pds-atmospheres.nmsu.edu/data_and_services/atmospheres_data/Mars/Mars.html
-#
 # Viking Landers
 #   Dataset:      VL1/VL2 MARS LCS DERIVED ATMOSPHERIC OPTICAL DEPTH V1.0      
 #   Description:  Viking Lander camera images of the Sun were used to compute total normal atmospheric
@@ -16,6 +10,12 @@
 #   Description:  Mars Exploration Rover Pancam Atmospheric Opacity for both the MER-1 and MER-2 rovers.
 #   Source:       PDS - Geosciences Node
 #   URL:          http://pds-geosciences.wustl.edu/missions/mer/geo_mer_datasets.htm
+#
+# Phoenix
+#   Dataset:      PHOENIX Atmospheric Opacity data.
+#   Desctiption:  Derived data from the SSI instrument measuring the atmospheric opacity for sols 1-151.
+#   Source:       PDS - Planetary Atmospheres Node
+#   URL:          https://pds-atmospheres.nmsu.edu/data_and_services/atmospheres_data/Mars/Mars.html
 #
 # Mars Science Laboratory (Curiosity)
 #   Dataset:  
@@ -42,14 +42,16 @@ tau_eq = dget(here("functions", "tau.R"))
 Ls_seq = seq(0, 360, 1) # [deg]
 phi_oppy = -2.28
 phi_spirit = -14.6
-model = 1 
-
+phi_vl1 = 22.27
+phi_vl2 = 47.64
+phi_phoenix = 68.22
+model = 1
 
 # For some reason not working when doing tau_eq(phi, Ls_seq, 1).
 # So get taus iteratively.
 tau_seq = c()
 for(Ls in Ls_seq){
-  tau = tau_eq(phi_oppy, Ls, model)
+  tau = tau_eq(phi_vl1, Ls, model)
   tau_seq = c(tau_seq, tau)
 }
 
@@ -63,78 +65,138 @@ plot(Ls_seq, tau_seq,
      main="Optical Depth as a Function of Areocentric Longitude",
      col="red")
 
-data_base_url = "http://pds-geosciences.wustl.edu/mer/mer1_mer2-m-pancam-5-atmos-opacity-v1/merao_1xxx/data/"
 
-# Opportunity, aka MER-B (Mars Exploration Rover – B) or MER-1.
-oppy_lbl_1 = paste(data_base_url, "1tau440_5106_20181207a.lbl", sep="")
-oppy_tab_1 = paste(data_base_url, "1tau440_5106_20181207a.tab", sep="")
-
-oppy_lbl_2 = paste(data_base_url, "1tau880_5106_20181207a.lbl", sep="")
-oppy_tab_2 = paste(data_base_url, "1tau880_5106_20181207a.tab", sep="")
-
-# Spirit, aka MER-A (Mars Exploration Rover – A) or MER-2.
-spirit_lbl_1 = paste(data_base_url, "2tau440_2209_20110214a.lbl", sep="")
-spirit_tab_1 = paste(data_base_url, "2tau440_2209_20110214a.tab", sep="")
-
-spirit_lbl_2 = paste(data_base_url, "2tau880_2209_20110214a.lbl", sep="")
-spirit_tab_2 = paste(data_base_url, "2tau880_2209_20110214a.tab", sep="")
-
-lbl = oppy_lbl_2 
-tab = oppy_tab_2
-
-# req = curl::curl_fetch_memory(lbl)
-# dat = rawToChar(req$content)
-# res = pds3_read(dat)
-# print(res$odl$DATA_SET_ID)
-
-
-conn = file(tab, open="r")
-lines = readLines(conn)
-close(conn)
-
-# Clean data.
-# Remove header.
-data_char = lines[seq(10, length(lines), 1)]
-# Remove whitespace and quotation marks
-data_char = gsub('\\s|\"', "", data_char, perl=TRUE)
-
-data = read.csv(text=data_char, header=FALSE)
-
-Ls_vect = unname(unlist(data[2]))
-Sols = unname(unlist(data[4]))
-taus = unname(unlist(data[7]))
-
-Ls_yr = c()
-Sols_yr = c()
-taus_yr = c()
-
-Ls_prev = 999
-year_count = 0
-index = 1
-for(Ls in Ls_vect){
-  # Handle negative values of Ls.
-  Ls = ifelse(Ls<0, Ls+360, Ls)
+tau_lines = function(Ls_vect, Sols, taus, sol_start, sol_end, include_years){
+  Ls_yr = c()
+  Sols_yr = c()
+  taus_yr = c()
   
-  # New year, Gone from Ls=360-ish to Ls=0-ish.
-  if(Ls < Ls_prev){
-    
-    if(!is.null(Ls_yr)){
-      lines(Ls_yr, taus_yr, type = "l",
-            col=wes_palette("FantasticFox1", 10, type="continuous")[year_count+1])
+  Ls_prev = 999
+  year_count = 0
+  index = 1
+  
+  for(sol in Sols){
+    if(sol >= sol_start && sol <= sol_end){
+      # Handle negative values of Ls.
+      Ls = ifelse(c(Ls_vect[index])<0, c(Ls_vect[index])+360, c(Ls_vect[index]))
+      
+      # New year, Gone from Ls=360-ish to Ls=0-ish.
+      if(Ls < Ls_prev){
+        
+        if(!is.null(Ls_yr)){
+          if(is.null(include_years) || year_count %in% include_years){
+            lines(Ls_yr, taus_yr, type = "l",
+                  col=wes_palette("FantasticFox1", 10, type="continuous")[year_count+1])
+          }
+        }
+        
+        Ls_yr = c(Ls)
+        Sols_yr = c(Sols[index])
+        taus_yr = c(taus[index])
+        
+        year_count = year_count + 1
+        
+      }else{
+        # Still in the same year.
+        Ls_yr = c(Ls_yr, Ls)
+        Sols_yr = c(Sols_yr, Sols[index])
+        taus_yr = c(taus_yr, taus[index])
+      }
+      
+      Ls_prev = Ls
+      index = index + 1
     }
-    Ls_yr = c(Ls)
-    Sols_yr = c(Sols[index])
-    taus_yr = c(taus[index])
+  }
+}
+
+mer_tau_lines = function(rover_id="MER-A", sol_start=0, sol_end=5106, include_years=NULL) {
+  mer_data_url = "http://pds-geosciences.wustl.edu/mer/mer1_mer2-m-pancam-5-atmos-opacity-v1/merao_1xxx/data/"
+  
+  # Opportunity, aka MER-B (Mars Exploration Rover – B) or MER-1.
+  oppy_lbl_1 = paste(mer_data_url, "1tau440_5106_20181207a.lbl", sep="")
+  oppy_tab_1 = paste(mer_data_url, "1tau440_5106_20181207a.tab", sep="")
+  
+  oppy_lbl_2 = paste(mer_data_url, "1tau880_5106_20181207a.lbl", sep="")
+  oppy_tab_2 = paste(mer_data_url, "1tau880_5106_20181207a.tab", sep="")
+  
+  # Spirit, aka MER-A (Mars Exploration Rover – A) or MER-2.
+  spirit_lbl_1 = paste(mer_data_url, "2tau440_2209_20110214a.lbl", sep="")
+  spirit_tab_1 = paste(mer_data_url, "2tau440_2209_20110214a.tab", sep="")
+  
+  spirit_lbl_2 = paste(mer_data_url, "2tau880_2209_20110214a.lbl", sep="")
+  spirit_tab_2 = paste(mer_data_url, "2tau880_2209_20110214a.tab", sep="")
+  
+  data_url = NULL
+  if(toupper(rover_id) %in% c("MER-A", "MERA", "MER-2", "MER2", "SPIRIT")){
+    data_url = spirit_tab_2
     
-    year_count = year_count + 1
+  }else if(toupper(rover_id) %in% c("MER-B", "MERB", "MER-1", "MER1", "OPPORTUNITY", "OPPY")){
+    data_url = oppy_tab_2
     
   }else{
-    # Still in the same year.
-    Ls_yr = c(Ls_yr, Ls)
-    Sols_yr = c(Sols_yr, c(Sols[index]))
-    taus_yr = c(taus_yr, c(taus[index]))
+    stop("Invalid input for rover_id, should either be 'MER-A' for Spirit or 'MER-B' for Opportunity.")
   }
   
-  Ls_prev = Ls
-  index = index + 1
+  conn = file(data_url, open="r")
+  lines = readLines(conn)
+  close(conn)
+  
+  # Clean the data so that it is compliant with CSV format.
+  # Remove header text.
+  data_char = lines[seq(10, length(lines), 1)]
+  
+  # Remove whitespace and quotation marks.
+  data_char = gsub('\\s|\"', "", data_char, perl=TRUE)
+  
+  # Read as CSV.
+  data = unname(read.csv(text=data_char, header=FALSE))
+  
+  Ls_vect = unlist(data[2])
+  Sols = unlist(data[4])
+  taus = unlist(data[7])
+  
+  tau_lines(Ls_vect, Sols, taus, sol_start, sol_end, include_years)
 }
+
+vl_tau_lines = function(lander_id="VL1", sol_start=0, sol_end=1147, include_years=NULL) {
+  vl_data_url = "https://atmos.nmsu.edu/pdsd/archive/data/vl1vl2-m-lcs-5-atmos-optical-depth-v10/vl_1001/data/vl_opac.dat"
+  
+  if(!(toupper(lander_id) %in% c("VL-1", "VL1", "VL-2", "VL2"))){
+    stop("Invalid input for rover_id, should either be 'MER-A' for Spirit or 'MER-B' for Opportunity.")
+  }
+    
+  conn = file(vl_data_url, open="r")
+  data_char = readLines(conn)
+  close(conn)
+  
+  # Clean the data so that it is compliant with CSV format.
+  # Put commas where there are whitespaces in between values.
+  data_char = substring(gsub("\\s+", ",", data_char, perl=TRUE), 2)
+  
+  # Read as CSV.
+  data = read.csv(text=data_char, header=FALSE)
+  
+  # Only grab data for the lander we are interested in.
+  data = subset(data, V1==gsub("-", "", lander_id))
+  
+  Ls_vect = unlist(data[10])
+  Sols = unlist(data[4])
+  taus = unlist(data[7])
+  
+  tau_lines(Ls_vect, Sols, taus, sol_start, sol_end, include_years)
+}
+
+phoenix_tau_lines = function(lander_id="VL1", sol_start=0, sol_end=1147, include_years=NULL) {
+}
+
+####################################################
+# Mars Exploration Rovers (Spirit and Opportunity) #
+####################################################
+mer_tau_lines(MER-A)
+
+##################
+# Viking Landers #
+##################
+#vl_tau_lines()
+
+
