5528cc2140e29a3e910782af6f44ac52669cd3a1
Georges Labrèche <georges@opendatakosovo.org>
2019-06-30 01:54:44 +0200

    Added comment.

--- test/test_H_obh.R
+++ test/test_H_obh.R
@@ -1,3 +1,12 @@
+# Equation 13 (1990): Daily beam insolation on a horizontal surfce at top of Mars atmosphere [Wh/m2-day].
+#
+# Based on equations presented in the following publication:
+#   Appelbaum, Joseph & Flood, Dennis. (1990). Solar radiation on Mars. Solar Energy. 45. 353–363. 10.1016/0038-092X(90)90156-7. 
+#   https://ntrs.nasa.gov/?R=19890018252
+#
+# Expected results taken from:
+#   TABLE II. - HOURLY AND DAILY BEAM INSOLATION ON A HORIZONTAL SURFACE AT TOP OF MARS ATMOSPHERE
+
 library(testthat) 
 library(here)
 
@@ -11,7 +20,7 @@ expected_results = list(
   "249" = 3449,
   "299" = 3350)
 
-test_that("Equation 12 (1990): H_obh.", {
+test_that("Equation 13 (1990): H_obh.", {
   
   index = 1
   for(Hobh_expected in expected_results){
