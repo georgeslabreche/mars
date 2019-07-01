bc3317539f45d1eda13007653ea22212630822aa
Georges Labrèche <georges@opendatakosovo.org>
2019-07-01 04:30:56 +0200

    Comments.

--- functions/H_h.R
+++ functions/H_h.R
@@ -1,4 +1,4 @@
-# Global daily insolation on Mars horizontal surface  [Wh/m2-day].
+# Global daily insolation on Mars horizontal surface [Wh/m2-day].
 #
 # Based on equations presented in the following publication:
 #   Appelbaum, Joseph & Flood, Dennis. (1990). Solar radiation on Mars. Solar Energy. 45. 353–363. 10.1016/0038-092X(90)90156-7. 
@@ -7,6 +7,7 @@
 
 library(here)
 
+# Global hourly insolation on Mars horizontal surface [W/m2-hr].
 Ih_eq = dget(here("functions", "I_h.R"))
 
 function(Ls, phi, tau, al=0.1, nfft){
