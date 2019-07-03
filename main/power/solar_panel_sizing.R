dda6cf19a55ba883e97de4ca0482b546ff4a6af5
Georges Labrèche <georges@opendatakosovo.org>
2019-07-03 04:48:07 +0200

    Solar panel sizing.

--- main/power/solar_panel_sizing.R
+++ main/power/solar_panel_sizing.R
@@ -1,5 +1,9 @@
+# InSight generated 4,588 watt-hours on its first sol.
+# Phoenix lander generated around 1,800 watt-hours in a day.
+
 library(here)
 library(whisker)
+library(wesanderson)
 
 # Equation 17 (1990): Global irradiance on Mars horizontal surface [W/m2].
 Gh_eq = dget(here("functions", "G_h.R"))
@@ -8,13 +12,12 @@ Gh_eq = dget(here("functions", "G_h.R"))
 Hh_eq = dget(here("functions", "H_h.R"))
 
 # Equation 6: Zenith angle of the incident solar radiation [deg].
-Z_eq = dget(here("functions", "Z.R")) 
+Z_eq = dget(here("functions", "Z.R"))
 
 # Daylight range.
 daylight_range = dget(here("utils", "daylight_range.R"))
 
-# InSight generated 4,588 watt-hours on its first sol.
-# Phoenix lander generated around 1,800 watt-hours in a day.
+E_cols = wes_palette("Darjeeling1", 4)
 
 # Define a class to encapsulate rover power consumption details.
 RoverPower = setClass(
@@ -68,7 +71,6 @@ phi = phi_list$'Nanedi Vallis'
 Ls_global_dust_storm_season = Ls_list$'Dust Storm Season Begins':Ls_list$'Dust Storm Season Ends'
 Ls_non_global_dust_storm_season = c(1:Ls_list$'Dust Storm Season Begins', Ls_list$'Dust Storm Season Ends':360)
 
-
 e = 29  # Solar panel efficiency [%].
 G = 0   # Solar flux / Irradiance [W/m2].
 
@@ -95,7 +97,7 @@ G = 0   # Solar flux / Irradiance [W/m2].
 PR = 1 - (0.03 + 0.05 + 0.30)
 
 # FIXME: During storm, we get different values if nfft=1 or nfft=2
-Gh_worst = function(Ls_range, Z, tau, al=0.1, nfft=2, verbose=TRUE){
+Gh_worst = function(Ls_range, Z, tau, al=0.1, nfft=3, verbose=FALSE){
   Gh_worst = 600
   Ls_worst = -1
   
@@ -120,7 +122,7 @@ Gh_worst = function(Ls_range, Z, tau, al=0.1, nfft=2, verbose=TRUE){
     "tau" = tau))
 }
 
-Hh_worst = function(Ls_range, phi, tau, al=0.1, nfft=3, verbose=TRUE){
+Hh_worst = function(Ls_range, phi, tau, al=0.1, nfft=3, verbose=FALSE){
   Hh_worst = 50000
   Ls_worst = -1
   
@@ -146,60 +148,90 @@ Hh_worst = function(Ls_range, phi, tau, al=0.1, nfft=3, verbose=TRUE){
     "tau" = tau))
 }
 
+get_worst_irradiance = function(A, Ls_range, phi, tau, nfft=3, verbose=FALSE){
+  # Get the worst insolation.
+  Hh_w = Hh_worst(Ls_range=Ls_range, phi=phi, tau=tau, nfft=nfft, verbose=verbose)
+  
+  # Get the daylight solar time range for the Ls with the worst insolation.
+  Ts_range = daylight_range(Ls=Hh_w$Ls, phi=phi, T_step=1)
+  
+  # Prep data matrix.
+  data_matrix = matrix(NA, nrow=4, ncol=length(Ts_range))
+  rownames(data_matrix) = c('Ts', 'Z', 'Gh', 'E')
+  
+  # Calculate diurnal values for Z, Gh, and E.
+  # TODO: Eventually calculate solar panel size for desired E and figure out cut off point
+  
+  Ts_index = 1
+  for(T_s in Ts_range){
+    Gh = Gh_eq(Ls=Hh_w$Ls, phi=phi, T_s=T_s, tau=tau, al=0.1, nfft=nfft)
+    Z = Z_eq(Ls=Hh_w$Ls, T_s=T_s, phi=phi, nfft=nfft)
+    
+    # Energy generated from solar panels [Wh].
+    E = A * e * Gh * PR
+    
+    #FIXME: First Gh is negative
+    data_matrix[1, Ts_index] = T_s
+    data_matrix[2, Ts_index] = Z
+    data_matrix[3, Ts_index] = Gh
+    data_matrix[4, Ts_index] = E
+    
+    Ts_index = Ts_index + 1
+  }
+  
+  return(data_matrix)
+}
+
 # Get the worst moment during a year without global storm
 tau = 0.5
 
-print('During a year with no global storms:')
-Hh_w1 = Hh_worst(Ls_range=1:360, phi, tau)
-Gh_w1 = Gh_worst(Ls_range=1:360, Z=0, tau=tau)
+#print('During a year with no global storms:')
+#Hh_w1 = Hh_worst(Ls_range=1:360, phi, tau)
+#Gh_w1 = Gh_worst(Ls_range=1:360, Z=0, tau=tau)
 
-print("")
+#print("")
 
 # Get worst moment during global storm season.
-tau = 5 # Jeremiah McNatt et al.
+#tau = 5 # Jeremiah McNatt et al.
 
 print('During global storms season:')
-Hh_w2 = Hh_worst(Ls_range=Ls_global_dust_storm_season, phi, tau)
-Gh_w2 = Gh_worst(Ls_range=Ls_global_dust_storm_season, Z=0, tau=tau)
+#Hh_w2 = Hh_worst(Ls_range=Ls_global_dust_storm_season, phi, tau)
+#Gh_w2 = Gh_worst(Ls_range=Ls_global_dust_storm_season, Z=0, tau=tau)
 
-Hh_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$Hh else Hh_w2$Hh
-Ls_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$Ls else Hh_w2$Ls
-tau_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$tau else Hh_w2$tau
+#Hh_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$Hh else Hh_w2$Hh
+#Ls_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$Ls else Hh_w2$Ls
+#tau_w = if(Hh_w1$Hh < Hh_w2$Hh) Hh_w1$tau else Hh_w2$tau
 
 # Total solar panel area [m2].
-A = 3
+A = 1
 
+###############################
+# Without global dust storms. #
+###############################
+Ls_range = 1:360
+tau = 1
+data_matrix = get_worst_irradiance(A, Ls_range, phi, tau, nfft=3, verbose=TRUE)
 
-# Get solar time range when there is daylight
-Ts_range = daylight_range(Ls=Ls_w, phi=phi, T_step=1)
-
-data_matrix = matrix(NA, nrow=4, ncol=length(Ts_range))
-rownames(data_matrix) = c('Ts', 'Z', 'Gh', 'E')
-
-Ts_index = 1
-for(T_s in Ts_range){
-  # Irradiance [W/m2].
-  Gh = Gh_eq(Ls=Ls_w, phi=phi, T_s=T_s, tau=tau_w, al=0.1, nfft=3)
-
-  # Energy generated from solar panels [Wh].
-  E = A * e * Gh * PR
-
-  #FIXME: First Gh is negative
-  data_matrix[1, Ts_index] = Ts
-  data_matrix[2, Ts_index] = Z
-  data_matrix[3, Ts_index] = Gh
-  data_matrix[4, Ts_index] = E
-
-  Ts_index = Ts_index + 1
-}
-
+# PLot
 dev.new()
-plot(round(Ts_range), data_matrix['E',],
+plot(round(data_matrix['Ts',]), data_matrix['E',],
      xlab="Solar Time, T [h]",
      ylab="Energy, E [Wh]",
      type="l",
+     col=E_cols[1],
      font.sub=2,
      cex.sub=1.2)
+
+
+############################
+# With global dust storms. #
+############################
+Ls_range = Ls_global_dust_storm_season
+tau = 5
+data_matrix = get_worst_irradiance(A, Ls_range, phi, tau, nfft=3, verbose=TRUE)
+
+lines(round(data_matrix['Ts',]), data_matrix['E',],
+      col=E_cols[2])
 
 #print(data_matrix)
 
